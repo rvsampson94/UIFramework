eb756f888357b8ad97dc1bbd671a840f9ca31c35
Morgan <morgan@localhost.localdomain>
2021-03-05 18:03:58 -0500

    Bind.jl

--- src/Bind.jl
+++ src/Bind.jl
@@ -6,19 +6,25 @@ bindings = Dict{Symbol, Array}()
 function get(key)
     return data[key]
 end
+
 function set(key, val)
     data[key] = val
-    if !haskey(bindings, key)
-        update(key)
-end
-function update(key1)
-    for key2 in data[key1]
-        set(key2, val)
+    if haskey(bindings, key)
+        update(key, val)
     end
 end
+
 function bind(key1, key2)
     if !haskey(bindings, key1)
         bindings[key1] = []
     end
     push!(bindings[key1], key2)
+end
+
+function update(key1, val)
+    for key2 in bindings[key1]
+        set(key2, val)
+    end
+end
+
 end
