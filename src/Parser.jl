1adc18addf4a9bc32d6a9c9fcde749d155d6a809
Morgan <morgan@localhost.localdomain>
2021-03-05 19:09:30 -0500

    better

--- src/Parser.jl
+++ src/Parser.jl
@@ -4,6 +4,8 @@ end
 
 tree = Meta.parse(code)
 
+dump(tree)
+
 function traverse(expr)
   for arg in expr.args
     if typeof(arg) !== Expr
@@ -14,11 +16,13 @@ function traverse(expr)
     elseif arg.head == :(=)
       arg.head = :call
       set = Expr(:., :Bind, QuoteNode(:set))
+      arg.args[1] = String(arg.args[1])
       prepend!(arg.args, [set])
     end
   end
 end
 
 traverse(tree)
-dump(tree)
+# dump(tree)
+print(tree)
 eval(tree)
